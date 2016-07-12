95724dc3606c6224bcc86e3b125fb9d970c5f8ec
tthsqe12 <tthsqe12@gmail.com>
2016-07-12 10:55:54 -0400

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,4 +1,7 @@
-"asmFish\fasm.exe" "asmFish\asmFishW_debug.asm"  -m 262144 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_debug.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_base.asm"   -m 262144 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_base.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_popcnt.asm" -m 262144 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_popcnt.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_bmi2.asm"   -m 262144 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_bmi2.exe"
+"asmFish\fasm.exe" "asmFish\asmFish_base.asm"   -m 262144 "Linux\asmFish_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_base"
+"asmFish\fasm.exe" "asmFish\asmFish_popcnt.asm" -m 262144 "Linux\asmFish_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_popcnt"
+"asmFish\fasm.exe" "asmFish\asmFish_bmi2.asm"   -m 262144 "Linux\asmFish_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_bmi2"
+
